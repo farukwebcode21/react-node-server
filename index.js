16a12f29ee7b6a065df3c69b0d2ed2d5e0b19666
farukwebcode21 <farukahmadmk@yahoo.com>
2021-11-16 16:24:12 +0600

    node-server

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const { MongoClient} = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+
+
+const app = express()
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdxsv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('foodCluster');
+        const usersCollection = database.collection('users');
+
+        //=========================GET API=============================
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+
+        })
+        //====================== GET Receive ID======================
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await usersCollection.findOne(query);
+            // console.log('loading id', id);
+            res.send(user);
+        })
+
+        //==================== Update API========================
+        app.put('/users/:id', async (req, res)=>{
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            console.log('updating user :', result);
+            res.json(result);
+        })
+
+        //======================== POST API=======================
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            res.json(result);
+        })
+
+
+        //========================= DELETE API========================
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            console.log('Successfully deleted one document.', result);
+            res.json(result)
+
+         })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    console.log('Server is Running:', port);
+})
